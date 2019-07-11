de98f35e4ae123adaa670a8c82e58ad31d1768fa
Justin Biele <justin.biele@verys.com>
2019-07-11 16:30:40 -0700

    chore: remove server.js contents

--- server.js
+++
@@ -1,9 +0,0 @@
-const app = require("express")()
-const PORT = process.env.PORT || 3001
-
-app.listen(PORT, err => {
-  if (err) {
-    console.error(err)
-  }
-  console.log(`  >> server listening on port: ${PORT}`)
-})
