cdc3d79fee4b1775c62996c419c781ba88b0e2eb
Justin Biele <justin.biele@verys.com>
2019-07-11 21:04:35 -0700

    feat: add user info to auth screen

--- client/src/screens/Auth/index.js
+++ client/src/screens/Auth/index.js
@@ -1,20 +1,52 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
+// import { User } from "../../api"
+import { Button, Input } from "../../components"
 
 export default function Auth({ location }) {
   const [register, toggleRegister] = useState(false)
+  const [{ email, name, password }, setAuthFields] = useState({
+    email: "",
+    name: "",
+    password: "",
+  })
 
-  const Login = () => (
+  const updateAuthFields = ({ target: { id, value } }) => {
+    setAuthFields({ [id]: value })
+  }
+
+  // const RegisterUser = () => User.register({ email, name, password })
+
+  const Login = (
     <div>
       <div>This is the login</div>
-      <button onClick={() => toggleRegister(!register)}>Go to register</button>
+      <Input id="email" onChange={updateAuthFields} value={email} />
+      <Input id="password" onChange={updateAuthFields} value={password} />
+      <div>
+        <Button primary onClick={() => toggleRegister(!register)}>
+          Login
+        </Button>
+        <Button primary onClick={() => toggleRegister(!register)}>
+          Go To Register
+        </Button>
+      </div>
     </div>
   )
-  const Register = () => (
+  const Register = (
     <div>
       <div>This is the register</div>
-      <button onClick={() => toggleRegister(!register)}>Go to login</button>
+      <Input id="email" onChange={updateAuthFields} value={email} />
+      <Input id="name" onChange={updateAuthFields} value={name} />
+      <Input id="password" onChange={updateAuthFields} value={password} />
+      <div>
+        <Button primary onClick={RegisterUser}>
+          Register
+        </Button>
+        <Button primary onClick={() => toggleRegister(!register)}>
+          Go To Login
+        </Button>
+      </div>
     </div>
   )
 
-  return <div>{register ? <Register /> : <Login />}</div>
+  return <div>{register ? Register : Login}</div>
 }
