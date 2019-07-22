1f74b7dad00b99328db05d8ff7322b7ea9a8685b
Qamar Stationwala <YoPappy0@gmail.com>
2019-07-22 14:37:46 -0700

    profile viewer/updater working. included placeholder buttons for opening trades and chats with other users when viewing a profile not your own

--- client/src/components/ViewProfile/index.js
+++ client/src/components/ViewProfile/index.js
@@ -2,16 +2,32 @@ import React from 'react';
 import Card from '@material-ui/core/Card';
 import CardHeader from '@material-ui/core/CardHeader';
 import CardContent from '@material-ui/core/CardContent';
-import IconButton from '@material-ui/core/IconButton';
-//import edit button from materialui
+import CardActions from '@material-ui/core/CardActions';
+import Button from '@material-ui/core/Button';
+import Avatar from '@material-ui/core/Avatar';
+import styles from './styles';
+import Typography from '@material-ui/core/Typography';
+import { User } from '../../api/';
+//import edit button from materialui icons
+//import some kind of icons for trades/chats
 
 const ViewProfile = (props) => {
-    return(
+    const { email, name, picture, rating, biography, userId } = props.user
+    const classes = styles()
+    return (
         <Card>
-            <CardContent>
-                {/* render user data here*/}
-                {/* conditional render edit button if given id matches id in localstorage */}
+            <CardHeader title={name} titleTypographyProps={{align: "center"}}/>
+            <CardContent className={classes.profileBody}>
+                <Avatar className={classes.avatar} src={picture} />
+                <Typography>{email}</Typography>
+                <Typography>Rating: {rating}</Typography>
+                <Typography>{biography}</Typography>
             </CardContent>
+            <CardActions className={classes.profileActions}>
+                {(userId === User.getUser().id) 
+                    ? <Button variant="contained" onClick={props.edit}>Edit</Button>
+                    : <><Button variant="contained">Trade</Button><Button variant="contained">Chat</Button></>}
+            </CardActions>
         </Card>
     );
 }
--- client/src/screens/Profile/index.js
+++ client/src/screens/Profile/index.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import Grid from '@material-ui/core/Grid';
 import queryStr from 'query-string';
-import CardList from '../../components/CardList';
+import { CardList, ViewProfile, EditProfile } from '../../components';
 import { User } from '../../api/';
 import styles from "./styles.js";
 const Profile = (props) => {
@@ -12,8 +12,9 @@ const Profile = (props) => {
         name: "",
         picture: "",
         rating: ""
-    })
-
+    });
+    const [loaded, setLoadedFlag] = useState(false);
+    const [editUser, setEditUserFlag] = useState(false);
     useEffect(() => {
         let query = queryStr.parse(props.location.search);
         let uId = (query.id) ? query.id : User.getUser().id;
@@ -25,12 +26,31 @@ const Profile = (props) => {
                     haves: data.haves,
                     wants: data.wants,
                     name: data.name,
+                    address: data.address,
                     picture: data.picture,
-                    rating: data.rating
+                    rating: data.rating,
+                    biography: data.biography,
+                    userId: uId
                 })
+                setLoadedFlag(true)
             })
             .catch(err => console.error(err));
-    }, [])
+    }, []);
+    const toggleEdit = () => {
+        setEditUserFlag(!editUser)
+    }
+    const setUserPic = (result) => {
+        if (result.filesFailed.length) {
+            console.error("Failed to upload image.");
+        } else {
+            setUserData({
+                ...userData,
+                picture: result.filesUploaded[0].url
+            })
+            console.log(result.filesUploaded[0].url)
+            console.log("Changing user pic");
+        }
+    }
     const classes = styles();
     return (
         <Grid className={classes.container} style={{ color: "white" }} container spacing={2}>
@@ -38,7 +58,11 @@ const Profile = (props) => {
                 <CardList editable={false} list={userData.haves} title="Haves" />
             </Grid>
             <Grid className={classes.profile} item xs={12} md={4}>
-                Profile
+                {(loaded)
+                    ? (editUser) 
+                        ? <EditProfile edit={toggleEdit} setUserPic={setUserPic} user={userData} />
+                        : <ViewProfile edit={toggleEdit} user={userData} /> 
+                    : ""}
             </Grid>
             <Grid className={classes.wants} item xs={12} md={4}>
                 <CardList editable={false} list={userData.wants} title="Wants" />
